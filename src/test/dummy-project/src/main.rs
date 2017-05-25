a53e0100b881c468e81e6a226a54a84651edaa44
Me <me@me.me>
2017-05-24 17:26:36 -0700

    Have dummy project use block doc comments.

--- src/test/dummy-project/src/main.rs
+++ src/test/dummy-project/src/main.rs
@@ -1,21 +1,23 @@
-//! Entry point for the dummy project.
-//! This comment block tests `rust doc`;
-//! you should be able to see it from the documentation
-//! folder after running the project's rustDoc task
-//! in Gradle.
+/*! Entry point for the dummy project.
+This comment block tests `rust doc`;
+you should be able to see it from the documentation
+folder after running the project's rustDoc task
+in Gradle.
+*/
 
-/// Prints a message to test documentation of
-/// functions.
-///
-/// # Parameters
-/// * `message`: The message to print.
-///
-/// # Examples
-/// This prints "Hi!":
-///
-/// ```
-/// sayHello("Hi!");
-/// ```
+/** Prints a message to test documentation of
+functions.
+
+# Parameters
+* `message`: The message to print.
+
+# Examples
+This prints "Hi!":
+
+```
+sayHello("Hi!");
+```
+*/
 pub fn sayHello(message: &str) {
 	println!("{}", message);
 }
